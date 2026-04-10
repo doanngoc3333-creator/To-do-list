86c67a0078b0d71985c70c9477b40979f93e6dd6
nogc <doanngoc3333@gmail.com>
2026-04-11 00:03:09 +0700

    Add mark done and delete, improve GUI

--- src/Todolist.java
+++ src/Todolist.java
@@ -14,13 +14,13 @@ class Task{ // class nay la work rieng le
     } // cai nay la setter nha
     public String toString(){ // cai nay in ra
         return todo;
-    }
+    } // in ra
     public void markDone(){
         this.done=true;
-    }
+    } // danh dau
     public boolean isDone(){
         return done;
-    }
+    } // tra ketqua
 }
 class MgrTodo { // cai nay la list work
     List<Task> todoList = new ArrayList<>();
@@ -45,6 +45,7 @@ public class Todolist{
     public static void main(String [] args){
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(Color.LIGHT_GRAY);
         JFrame frame = new JFrame();
         frame.setSize(1080,720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,15 +61,37 @@ public class Todolist{
                 todo.addTodo(text);
             }
             panel.removeAll();
-            for(Task t: todo.getTodoList()){
-                JLabel label = new JLabel(t.toString());
-                panel.add(label);
+            for(int i=0;i<todo.getTodoList().size();i++){
+                Task t = todo.getTodoList().get(i);
+                JPanel taskPanel = new JPanel(); // tao task o day nha
+                taskPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+                JLabel task = new JLabel(t.toString());
+                taskPanel.add(task);
+                JButton delete = new JButton("X");
+                int index = i;
+                delete.addActionListener( e1->{
+                    todo.deleteTodo(index);
+                    button.doClick();
+                });
+                JButton tick = new JButton("V");
+                int index1 = i;
+                tick.addActionListener( e2->{
+                    todo.markDone(index1);
+                    button.doClick();
+                });
+                taskPanel.add(delete);
+                taskPanel.add(tick);
+                panel.add(taskPanel);
+                panel.add(Box.createVerticalStrut(1));
+                taskPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
             }
             panel.revalidate();
             panel.repaint();
             input.setText("");
                 });
-        frame.add(inputPanel,BorderLayout.EAST);
+        inputPanel.add(input,BorderLayout.EAST);
+        inputPanel.add(button,BorderLayout.WEST);
+        frame.add(inputPanel,BorderLayout.NORTH);
         frame.add(panel,BorderLayout.CENTER);
         frame.setVisible(true);
     }
